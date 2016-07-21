a56decb5dce1a702a873450dee83a942af0edc3b
gregoryhooks <gregoryhooksjr@gmail.com>
2016-07-21 13:43:26 -0500

    Added basic API functions

--- WebContent/models/OCI_API.class.php
+++ WebContent/models/OCI_API.class.php
@@ -4,14 +4,67 @@ class OCI_API {
 	public static function getCatalog() {
 		$ch = curl_init();
 		
-		curl_setopt($ch, CURLOPT_URL, "https://129.114.110.199:8000/catalog/");
+		curl_setopt($ch, CURLOPT_URL, "http://129.114.110.218:12345/catalog/");
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		
 		$response = curl_exec($ch);
 		
 		curl_close($ch);
 		
-		print_r($response);
+		return($response);
+	}
+	
+	public static function registerUser($user, $userData) {
+	
+		$json = array("api_uname" => "webportal", 
+						"api_pass" => "greg123",
+						"username" => $user->getUserName(), 
+						"email" => $userData->getEmail(), 
+						"preferred_pass" => $userData->getVMPassword(), 
+						"external_id" => $user->getUserId());
+		$json = json_encode($json);
+	
+		$ch = curl_init();
+	
+		curl_setopt($ch, CURLOPT_URL, "http://129.114.110.218:12345/register/");
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+				'Content-Type: application/json',
+				'Content-Length: ' . strlen($json))
+				);
+	
+		$response = curl_exec($ch);
+	
+		curl_close($ch);
+	
+		return($response);
+	}
+	
+	public static function getInstances($userID) {
+		
+		$json = array("api_uname" => "webportal", 
+						"api_pass" => "greg123", 
+						"userid" => $userID);
+		$json = json_encode($json);
+		
+		$ch = curl_init();
+	
+		curl_setopt($ch, CURLOPT_URL, "http://129.114.110.218:12345/lablist/");
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    		'Content-Type: application/json',
+    		'Content-Length: ' . strlen($json))
+		);
+	
+		$response = curl_exec($ch);
+	
+		curl_close($ch);
+	
+		return($response);
 	}
 }
 ?>
--- WebContent/tests/OCI_API_tests.php
+++ WebContent/tests/OCI_API_tests.php
@@ -7,12 +7,41 @@
 
 <?php
 include_once("../models/OCI_API.class.php");
+include_once("../models/User.class.php");
+include_once("../models/UserData.class.php");
+include_once("../models/Messages.class.php");
+
+set_time_limit(120);
 ?>
 
 <h2>It should grab the course catalog</h2>
 <?php 
 
-OCI_API::getCatalog();
+$catalog = OCI_API::getCatalog();
+print_r($catalog);
+
+?>
+
+<h2>It should register a user </h2>
+<?php 
+
+$validTest = array("userName" => "ghooks", "password" => "test");
+$s1 = new User($validTest);
+$s1->setUserId(4);
+
+$validTest = array("email" => "test@gdail.com",
+		"vmPassword" => "testpassword"
+);
+$s2 = new UserData($validTest);
+
+OCI_API::registerUser($s1, $s2);
+
+?>
+
+<h2>It should retrieve a list of instances for a user</h2>
+<?php 
+$instances = OCI_API::getInstances($s1->getUserId());
+print_r($instances);
 
 ?>
 
