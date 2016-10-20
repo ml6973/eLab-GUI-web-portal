6f6bb44144415735962132e3f2e241aac7b6cab8
Jarred Deeley <j.mcknight.d@gmail.com>
2016-10-20 12:11:17 -0500

    Added organization image to courseDetails

--- WebContent/views/CourseDetailsView.class.php
+++ WebContent/views/CourseDetailsView.class.php
@@ -11,11 +11,12 @@ class CourseDetailsView {
   	$base = $_SESSION['base'];
   	$pathDir = dirname(__FILE__);  //Initialize the path directory
 
-    $fullPath = $pathDir . DIRECTORY_SEPARATOR . "../resources/marketPlaceData/";
+    $fullPath = $pathDir . DIRECTORY_SEPARATOR . "..".DIRECTORY_SEPARATOR."resources".DIRECTORY_SEPARATOR."marketPlaceData".DIRECTORY_SEPARATOR;
+
 
     // Get the course directory
   	if (file_exists($fullPath) && is_dir($fullPath)){
-        $courseDir = "/machinelearning-paul_rad";
+        $courseDir = $course;
   	} else {
         echo 'invalid course';
     }
@@ -31,9 +32,26 @@ class CourseDetailsView {
             </head>
 
         	<!--Banner-->
-        	<div class="jumbotron" style="background-image: url(/eLab-GUI-web-portal/resources/siteImages/classRoom.jpg);">
+        	<div class="jumbotron" style="background-image: url(/eLab-GUI-web-portal/resources/marketPlaceData/'.$courseDir.'/thumbnail.jpg);">
                 <div class="courseTitle"><h1>'.$courseYaml[0]['title'].'</h1></div>
         	</div>';
+
+            $organizationImage = preg_replace('/\s/', '_', $courseYaml[0]['organization']).".jpg";
+            $organizationImagePath = $pathDir . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "resources" . DIRECTORY_SEPARATOR . "organizations" .DIRECTORY_SEPARATOR. $organizationImage;
+            if (file_exists($organizationImagePath)) {
+                echo '
+                <div class="organization">
+                    <p>Created by:
+                    <img id = "organizationImage" src="/'.$base.'/resources/organizations/'.$organizationImage.'" alt="'.$courseYaml[0]['organization'].'"></p>
+                </div>
+                ';
+            } else {
+                echo '
+                <div class="organization">
+                    <p>Created by: '.$courseYaml[0]['organization'].'</p>
+                </div>
+                ';
+            }
 
             echo '<div class="courseDetails">';
             $ParseDown = new ParsedownExtra();
@@ -42,92 +60,9 @@ class CourseDetailsView {
             echo '</div>';
 
             //Footer
-            echo '
-            <div class="organization">
-                <p>'.$courseYaml[0]['organization'].'</p>
-            </div>
-            ';
         }
     }
 
-
-  /*	$fullPath = $pathDir . DIRECTORY_SEPARATOR . "../resources/customCourseData/";
-
-  	if (file_exists($fullPath) && is_dir($fullPath)){
-  		$files = scandir($fullPath);
-  		$files = array_diff($files, array('.', '..'));
-  	}
-
-  	foreach($files as $file) {
-
-  		$courseYaml = Spyc::YAMLLoad($fullPath.$file);
-
-  		if (!is_null($instances) && array_key_exists($courseYaml[0]['image'], $instances)) {
-	  		echo '<div class="container">
-				<h2 class="text-left">'.$courseYaml[0]['title'].'</h2><br>';
-	  			if (is_array($courseYaml[0]["description"])){
-	  				foreach ($courseYaml[0]["description"] as $sentence) {
-	  					echo '<p>'.$sentence.'</p>';
-	  				}
-	  			}else
-	  				echo '<p>'.$courseYaml[0]["description"].'</p>';
-				echo '<br><div class="col-md-8">';
-	  		echo '
-			    <br><br>
-				</div>
-				<div class="col-md-3" ng-include>';
-	  		if (!is_null($instances) && array_key_exists($courseYaml[0]['image'], $instances))
-	  			vmInfo::showCustom($courseYaml[0]['image'], $courseYaml[0]['type']);
-	  			echo '</div>
-			</div>';
-  		}
-  	}
-
-  	$fullPath = $pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/courses/";
-
-  	if (file_exists($fullPath) && is_dir($fullPath)){
-  		$files = scandir($fullPath);
-  		$files = array_diff($files, array('.', '..'));
-  		sort($files, SORT_REGULAR | SORT_NATURAL);
-  	}
-
-  	foreach($files as $file) {
-
-  		$courseYaml = Spyc::YAMLLoad($fullPath.$file);
-
-	  	echo '<div class="container">
-			<h2 class="text-left">'.$courseYaml[0]['title'].'</h2>
-		  <p>'.$courseYaml[0]['description'].'</p>
-			<br>
-			<div class="col-md-8">';
-				$fileName = $pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/topics/" . $courseYaml[0]['topicFile'];
-				$yaml = Spyc::YAMLLoad($fileName);
-				foreach($yaml as $topic) {
-					echo '
-		           <ul>
-			        	<div>';
-					if (!is_null($instances) && array_key_exists($courseYaml[0]['image'], $instances))
-				    		echo '<a href="topics?'.$topic["link"].'" >  <h3> - '.$topic["title"].'</h3></a>';
-					else
-							echo '<h3> - '.$topic["title"].'</h3>';
-				    echo '<!--description
-		            <p>'.$topic["description"].'</p>
-		            -->
-				    	</div>
-		          </ul>';
-				}
-				echo '
-		    	<br><br>
-			</div>
-			<div class="col-md-3" ng-include>';
-				if (!is_null($instances) && array_key_exists($courseYaml[0]['image'], $instances))
-					vmInfo::show($courseYaml[0]['image']);
-				else
-					vmInfo::showDisabled();
-			echo '</div>
-		</div>';
-  	}
-  	*/
   }
 }
 ?>
