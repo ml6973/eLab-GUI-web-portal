a48483ba3216911b0858c1af609c7f1126e04f43
gregoryhooks <gregoryhooksjr@gmail.com>
2016-12-01 23:07:14 -0600

    Added course, topic, and post import functionality

--- WebContent/tests/makeMongo.php
+++ WebContent/tests/makeMongo.php
@@ -38,16 +38,102 @@ function makeMongo() {
 		}
 	}
     
+	//Test retrieval for marketplace objects
     $results = $courses->find( array("identifier" => "marketPlaceObject") );
     foreach ($results as $course) {
     	print_r($course);
-    	print "<br>";
+    	echo "<br>";
     	echo '<img src="data:image/jpg;base64,'.base64_encode($gridFS->findOne(array("_id" => $course['thumbnail']))->getBytes()).'">';
-    	print "<br>";
+    	echo "<br>";
     	$ParseDown = new ParsedownExtra();
     	echo $ParseDown->text($gridFS->findOne(array("_id" => $course['details']))->getBytes());
-    	print "<br><br><br>";
+    	echo "<br><br><br>";
     }
+    
+    //Go through each course file and create the appropriate mongo documents
+    $fullPath = $pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/courses/";
+    if (file_exists($fullPath) && is_dir($fullPath)){
+    	$files = scandir($fullPath);
+    	$files = array_diff($files, array('.', '..'));
+    	sort($files, SORT_REGULAR | SORT_NATURAL);
+    }
+    foreach($files as $file) {
+    	$courseYaml = Spyc::YAMLLoad($fullPath.$file);
+    	if (!array_key_exists("type", $courseYaml[0])) {
+    		$courseYaml[0]['type'] = "ssh";
+    	}
+    	if (!array_key_exists("lessonlink", $courseYaml[0])) {
+    		$courseYaml[0]['lessonlink'] = "";
+    	}
+    	
+    	$topicsArray = array();
+    	$topics = Spyc::YAMLLOAD($pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/topics/" . $courseYaml[0]['topicFile']);
+    	foreach ($topics as $topic) {
+    		$postsArray = array();
+    		$postPath = $pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/posts/" . $topic['link'] . "/";
+    		$posts = scandir($postPath);
+    		$posts = array_diff($posts, array('.', '..'));
+    		sort($posts, SORT_REGULAR | SORT_NATURAL);
+    		foreach ($posts as $post) {
+    			if (preg_match('/\.md$/', $post)) {
+	    			$postContents = preg_split('/---/', file_get_contents($postPath.$post), 3);
+					$postAttributes = preg_replace('/:[\s\t]/', ':', $postContents[1]);
+					$postAttributes = preg_replace('/[\n\r]+/', "\n", $postAttributes);
+					$postAttributes = preg_split('/[:\n\r]/', $postAttributes);
+					if (empty($postAttributes[0]))
+						$number = 4;
+					else
+						$number = 3;
+					
+					$postID = $gridFS->storeBytes($postContents[2]);
+					$postObject = array(
+							'identifier' => "postObject",
+							'title' => $postAttributes[$number],
+							'description' => $postAttributes[$number+6],
+							'author' => $postAttributes[$number+4],
+							'post' => $postID
+					);
+					$courses->save($postObject);
+					array_push($postsArray, $postObject['_id']);
+    			}
+    		}
+    		
+    		$topicObject = array(
+    				'identifier' => "topicObject",
+    				'title' => $topic['title'],
+    				'description' => $topic['description'],
+    				'posts' => $postsArray
+    		);
+    		$courses->save($topicObject);
+    		array_push($topicsArray, $topicObject['_id']);
+    	}
+    	
+    	$courseObject = array(
+    			'identifier' => "courseObject",
+    			'title' => $courseYaml[0]['title'],
+    			'description' => $courseYaml[0]['description'],
+    			'image' => $courseYaml[0]['image'],
+    			'type' => $courseYaml[0]['type'],
+    			'link' => $courseYaml[0]['lessonlink'],
+    			'topics' => $topicsArray
+    	);
+    	$courses->save($courseObject);
+    }
+    
+    //Print one course for relationship verification
+    $result = $courses->findOne( array("identifier" => "courseObject") );
+    echo "<br><br><br>";
+    print_r($course);
+    echo "<br>";
+    $topic = $courses->findOne( array('_id' => $result['topics'][0]) );
+    print_r($topic);
+    echo "<br>";
+    $post = $courses->findOne( array('_id' => $topic['posts'][0]) );
+    print_r($post);
+    echo "<br>";
+    $ParseDown = new ParsedownExtra();
+    echo $ParseDown->text($gridFS->findOne(array("_id" => $post['post']))->getBytes());
+    echo "<br><br><br>";
 }
 
 ?>
