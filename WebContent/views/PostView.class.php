2b36d1227cfb0d82e316263ac43d2c0fddbb6a09
Jarred Deeley <j.mcknight.d@gmail.com>
2016-12-02 23:36:03 -0600

    Modified PostView to use mongoDB

--- WebContent/views/PostView.class.php
+++ WebContent/views/PostView.class.php
@@ -10,44 +10,47 @@ class PostView {
   public static function showDetails($topic, $post) {
   	$base = $_SESSION['base'];
   	$number = 0;
-  	$pathDir = dirname(__FILE__);  //Initialize the path directory
-  	$fullPath = $pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/posts/" . $topic . "/";
-  	if (!(file_exists($fullPath) && file_exists($fullPath.$post)) || empty($post) || !is_file($fullPath.$post)) {
-  		$topic = null;
-  		$post = null;
+  	
+  	//Get connection and select the collection
+  	$db = MongoDatabase::getConnection();
+  	$courses = $db->selectCollection('courseData');
+  	$gridFS = $db->getGridFS();
+  	
+  	//Build Object IDs from parameter
+  	$topicObjectId = null;
+  	$postObjectId = null;
+  	try {
+  		$topicObjectId = new MongoId($topic);
+  		$postObjectId = new MongoId($post);
+  	} catch (MongoException $e) {
+  		$topicObjectId = null;
+  		$postObjectId = null;
   	}
+  	
+  	$topicObject = $courses->findOne( array("_id" => $topicObjectId) );
+  	$postObject = $courses->findOne( array("_id" => $postObjectId) );
  
 	echo '<div class=\'container\'>
 <div class=\'row centered\'>
 		<div class="col-md-1"></div> 
 		<div class="col-md-8">';
-			if (!(is_null($topic) || is_null($post))){
+			if (!(is_null($topicObject) || is_null($postObject))){
 				//echo '<a href=\'/'.$base.'/topics?'.$topic.'\'><h2>Go back to topic</h2></a>';
 				echo '<a href=\'/'.$base.'/topics?'.$topic.'\' class="btn btn-info" id="homebutton" ng-click=\'/'.$base.'/topics?'.$topic.'\' ng-hide=[[buttonShow]] style="font-size:2.5rem;"><i class="fa fa-arrow-circle-o-left" aria-hidden="true"></i> Go Back</a>';
 			}
 			//echo '<a href=\'/'.$base.'/courses\'><h2>Go back home</h2></a>
 			echo '<a href="/'.$base.'/courses" class="btn btn-info pull-right" id="backbutton" ng-click=\'/'.$base.'/courses\' ng-hide=[[buttonShow]] style="font-size:2.5rem;"><i class="fa fa-home" aria-hidden="true"></i> Go Home</a>
 			<div>';
-				if (is_null($topic) || is_null($post)) {
+				if (is_null($topicObject) || is_null($postObject)) {
 					echo '<div><h3>No Post Selected</h3></div>';
 				}else {
-					$postContents = preg_split('/---[\r\n]/', file_get_contents($fullPath.$post), 3);
-					$postAttributes = preg_replace('/:[\s\t]/', ':', $postContents[1]);
-					$postAttributes = preg_replace('/[\n\r]+/', "\n", $postAttributes);
-					$postAttributes = preg_split('/[:\n\r]/', $postAttributes);
-					$fileName = $pathDir . DIRECTORY_SEPARATOR . "../resources/courseData/videos/videos.yaml";
-					$yaml = Spyc::YAMLLoad($fileName);
-					if (empty($postAttributes[0]))
-						$number = 4;
-					else
-						$number = 3;
-					echo '<h1>'.$postAttributes[$number].'</h1>';
-					foreach($yaml as $topic) {
-						if (strcmp($topic['title'], $postAttributes[$number]) == 0)
-							echo $topic['code'];
+
+					echo '<h1>'.$postObject['title'].'</h1>';
+					if (!empty($postObject['video'])) {
+						echo '<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="'.$postObject['video'].'" frameborder="0" allowfullscreen></iframe></div>';
 					}
 					$ParseDown = new ParsedownExtra();
-					echo $ParseDown->text($postContents[2]);
+					echo $ParseDown->text($gridFS->findOne(array("_id" => $postObject['post']))->getBytes());
 				}
 			echo '</div>
 		</div>
