44b5e109a08d6c6b3659fe70093dd3cb7d4fa111
Daniel Curran <daniel.curran@rackspace.com>
2017-06-20 22:19:05 +0000

    updating long tests script

--- tests/long_tests.sh
+++ tests/long_tests.sh
@@ -1,1 +1,13 @@
-echo "NEED TO WRITE LONG TESTS"
+cp /var/www/myConfig.ini ~
+cd ~/eLab-GUI-web-portal/unit/models
+
+phpunit DatabaseTest.php
+phpunit DBMaker.php
+phpunit DBMakerTest.php
+phpunit MessagesTest.php
+phpunit UserDataDBTest.php
+phpunit UserDataTest.php
+phpunit UsersDBTest.php
+phpunit UserTest.php
+
+rm ~/myConfig.ini
