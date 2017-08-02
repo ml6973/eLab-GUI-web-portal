c66d3af376e51d9e5942cc01466cf167faf5b07e
Daniel Curran <danielcurran90@gmail.com>
2017-08-02 18:46:34 -0500

    Update long_tests.sh

--- tests/long_tests.sh
+++ tests/long_tests.sh
@@ -3,19 +3,19 @@ cd unit/models
 
 echo "------DatabaseTest.php-----"
 phpunit DatabaseTest.php
-echo "------DatabaseTest.php-----"
+echo "------DBMaker.php-----"
 phpunit DBMaker.php
-echo "------DatabaseTest.php-----"
+echo "------DBMakerTest.php-----"
 phpunit DBMakerTest.php
-echo "------DatabaseTest.php-----"
+echo "------MessagesTest.php-----"
 phpunit MessagesTest.php
-echo "------DatabaseTest.php-----"
+echo "------UserDataDBTest.php-----"
 phpunit UserDataDBTest.php
-echo "------DatabaseTest.php-----"
+echo "------UserDataTest.php-----"
 phpunit UserDataTest.php
-echo "------DatabaseTest.php-----"
+echo "------UsersDBTest.php-----"
 phpunit UsersDBTest.php
-echo "------DatabaseTest.php-----"
+echo "------UserTest.php-----"
 phpunit UserTest.php
 
 
