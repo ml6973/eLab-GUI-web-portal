da573d7e2e74dfe3360f933de0c4dc3778ff055a
Daniel Curran <danielcurran90@gmail.com>
2017-08-02 18:45:25 -0500

    Update long_tests.sh

--- tests/long_tests.sh
+++ tests/long_tests.sh
@@ -1,13 +1,22 @@
 cp /var/www/myConfig.ini ..
 cd unit/models
 
+echo "------DatabaseTest.php-----"
 phpunit DatabaseTest.php
+echo "------DatabaseTest.php-----"
 phpunit DBMaker.php
+echo "------DatabaseTest.php-----"
 phpunit DBMakerTest.php
+echo "------DatabaseTest.php-----"
 phpunit MessagesTest.php
+echo "------DatabaseTest.php-----"
 phpunit UserDataDBTest.php
+echo "------DatabaseTest.php-----"
 phpunit UserDataTest.php
+echo "------DatabaseTest.php-----"
 phpunit UsersDBTest.php
+echo "------DatabaseTest.php-----"
 phpunit UserTest.php
 
-rm ~/myConfig.ini
+
+rm ../../../myConfig.ini
